f5b1690be691e41d6fbd0d99d539e3f027051c8a
dark phoenix <akshit2022@outlook.com>
2023-12-23 16:21:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  https://github.com/darkphoenix2601/LazyPrincessV2
+  https://github.com/darkphoenix2601/idj
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2
+  git clone $UPSTREAM_REPO /idj
 fi
-cd /LazyPrincessV2
+cd /idj
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
